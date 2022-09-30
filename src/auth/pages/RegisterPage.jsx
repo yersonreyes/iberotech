a7080544acea6272dbfccfe01f6d98f32f2d5432
Yerson Reyes <yerson.programador@gmail.com>
2022-09-30 09:43:00 -0300

    [add] login and register styles

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,77 @@
-import React from "react";
+import { Button, Grid, TextField, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow animate__animated animate__fadeIn animate__faster"
+        xs={3}
+        sx={{
+          width: { sm: 450 },
+          backgroundColor: "white",
+          padding: 3,
+          borderRadius: 2,
+        }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Registrate
+        </Typography>
+        <form action="">
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder="correo@correo.com"
+                fullWidth
+                name="email"
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="contraseña"
+                fullWidth
+                name="password"
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="contraseña"
+                fullWidth
+                name="password"
+              />
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained" fullWidth>
+                  Registrar
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/login">
+                Ya tienes una cuenta?
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
