a99018764b68a957b74d6006bfe6159218e8feda
Yerson Reyes <yerson.programador@gmail.com>
2022-10-11 11:23:00 -0300

    [add] ChekingAuth component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { DashboardRoutes } from "../dashboard/routes/DashboardRoutes";
+import { CheckingAuth } from "../ui/CheckingAuth";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
   return (
     <Routes>
       <Route path="/*" element={<DashboardRoutes />} />
